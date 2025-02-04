14f70ef861198f013a4c9abe28f3f3fff2d0a8ba
Ian Massingham <ian@griptape.ai>
2025-02-04 08:40:16 -0800

    move structureRun into utils as a function

--- src/app.js
+++ src/app.js
@@ -1,30 +1,11 @@
-const axios = require('axios');
+const { runStructure } = require('./utils/runstructure');
 require('dotenv').config();
-const { createStructureRun, pollEventEndpoint, getStructureRunOutput } = require('./utils/api');
 
 const main = async () => {
     try {
-        // Initialize the Griptape API client and create a structure run
-        const runId = await createStructureRun({"args": ['write me a poem']});
+        const results = await runStructure('write me a poem');
 
-        console.log('Run created:', runId);
-        
-        // Poll the event endpoint for the status of the run
-        let result;
-        let finished = false;
-        let offset = 0;
-        do {
-            result = await pollEventEndpoint(runId, offset);
-            offset = result.next_offset;
-            await new Promise(resolve => setTimeout(resolve, 250)); // Poll every 5 seconds
-            if (result.events.length > 0) { 
-                if (result.events[result.events.length - 1].type === 'StructureRunCompleted') {finished = true};
-            }
-        } while (finished === false);
-        
-        const output = await getStructureRunOutput(runId);
-
-        console.log('Output:', output.output.value);
+        console.log('Output:', results);
     } catch (error) {
         console.error('Error:', error.message);
     }
---
+++ src/utils/runstructure.js
@@ -0,0 +1,36 @@
+const axios = require('axios');
+require('dotenv').config();
+const { createStructureRun, pollEventEndpoint, getStructureRunOutput } = require('./api');
+
+async function runStructure(prompt) {
+    try {
+        // Initialize the Griptape API client and create a structure run
+        const runId = await createStructureRun({"args": [prompt]});
+
+        console.log('Run created:', runId);
+        
+        // Poll the event endpoint for the status of the run
+        let result;
+        let finished = false;
+        let offset = 0;
+        do {
+            result = await pollEventEndpoint(runId, offset);
+            offset = result.next_offset;
+            await new Promise(resolve => setTimeout(resolve, 250)); // Poll every 5 seconds
+            if (result.events.length > 0) { 
+                if (result.events[result.events.length - 1].type === 'StructureRunCompleted') {finished = true};
+            }
+        } while (finished === false);
+        
+        const output = await getStructureRunOutput(runId);
+
+        return output.output.value;
+
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+};
+
+module.exports = {
+    runStructure
+};
